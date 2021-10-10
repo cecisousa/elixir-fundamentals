e09632c9c46a42c80ea3030367e4da54e006a612
Cecilia Sousa <cecilia.sousa@sumup.com>
2021-10-10 20:55:05 -0300

    Refactor recursion with better namings

--- loop.exs
+++ loop.exs
@@ -1,11 +1,11 @@
 defmodule NewModule.Loops do
     def multiply(multiplier) do
-        multiply(multiplier, 1)
+        multiplying(multiplier, 1)
     end
 
-    defp multiply(_, 11), do: nil
-    defp multiply(first_number, second_number) do
+    defp multiplying(_, 11), do: nil
+    defp multiplying(first_number, second_number) do
         IO.puts("#{first_number} x #{second_number} = #{first_number * second_number}")
-        multiply(first_number, second_number + 1)
+        multiplying(first_number, second_number + 1)
     end
 end
