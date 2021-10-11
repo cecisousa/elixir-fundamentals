06056397f54fcfcf3713066d559c6064879cf4d0
Cecilia Sousa <cecilia.sousa@sumup.com>
2021-10-10 21:26:35 -0300

    Add guard clause example by using when

--- math.exs
+++ math.exs
@@ -2,4 +2,7 @@ defmodule NewModule.Math do
     def sum(param1, param2) do
         param1 + param2
     end
+
+    def zero?(0), do: true
+    def zero?(number) when(is_integer(number)), do: false
 end
